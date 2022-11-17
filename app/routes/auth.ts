15c5cf9c09d1a7d726c55fa7bc2f30ed64b56a33
Eng. José Pedro Silva <eng.josepsilva@gmail.com>
2022-11-17 15:29:50 +0000

    Swagger

--- app/routes/auth.ts
+++ app/routes/auth.ts
@@ -2,6 +2,64 @@ import express from 'express';
 import controller from '../controllers/auth';
 const _router = express.Router();
 
+  /**
+   * @swagger
+   * components:
+   *   schemas:
+   *     User:
+   *       type: object
+   *       required:
+   *         - nome
+   *       properties:
+   *         id:
+   *           type: integer
+   *           description: ID auto-gerado
+   *         name:
+   *           type: string
+   *           description: nome do utilizador
+   *         email:
+   *           type: string
+   *           description: email do utilizador
+   *         password:
+   *           type: string
+   *           description: password do utilizador
+   *       example:
+   *         name: "Super User"
+   *         email: "SU@email.com"
+   *         password: "ere+rwrewrw"
+   *
+   */
+
+    /**
+   * @swagger
+   *  tags:
+   *    name: User
+   *    description: User existentes na aplicação
+   */
+
+  /**
+   * @swagger
+   * /signup:
+   *   post:
+   *     summary: Criar um novo utilizador
+   *     tags: [User]
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/User'
+   *     responses:
+   *       200:
+   *         description:
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/User'
+   *       500:
+   *         description: Some server error
+   */
+
 _router.post('/signup', controller.signup);
 //_router.post('/login', controller.login);
 
--- app/server.ts
+++ app/server.ts
@@ -3,6 +3,8 @@ import express , {Express} from 'express';
 import morgan from 'morgan';
 import { router as PostRouter } from './routes/posts';
 import { router as AuthRouter } from './routes/auth';
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from 'swagger-jsdoc';
 
 import mongoose from 'mongoose';
 import mongoConfig from './config/mongo';
@@ -17,6 +19,40 @@ const router: Express = express();
 router.use(express.urlencoded({extended: false}));
 
 router.use(express.json());
+
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "API Formação",
+        version: "0.1.0",
+        description:
+          "This is a simple CRUD API",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+        contact: {
+          name: "Eng. José Silva",
+          url: "https://zepedro.vercel.app",
+          email: "info@email.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:8080/",
+        },
+      ],
+    },
+    apis: ["./app/routes/*.ts"],
+  };
+
+  const specs = swaggerJSDoc(options);
+router.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 
 router.use('/', PostRouter);
